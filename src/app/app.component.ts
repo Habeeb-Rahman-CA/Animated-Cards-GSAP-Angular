240e5d0c4286f91bec27e48c788d7c7e1be61c60
HabeebR07 <habeebrahmanca07@gmail.com>
2024-11-15 11:23:05 +0530

    done with some html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Animated-Cards-GSAP-Angular';
+  
+  data = [
+    {
+      title: 'Total Orders',
+      color: 'lightpurple',
+      amount: 313.12,
+      percent: 45,
+      iconName: 'icon-shop'
+    },
+    {
+      title: 'Total Views',
+      color: 'lightgrey',
+      amount: 120.50,
+      percent: 47,
+      iconName: 'icon-shopping-cart'
+    },
+    {
+      title: 'Conversation Rate',
+      color: 'lightgreen',
+      amount: 98.3,
+      percent: 78,
+      iconName: 'icon-bar-graph'
+    },
+    {
+      title: 'Avg Orders',
+      color: 'lightorange',
+      amount: 260.32,
+      percent: 64,
+      iconName: 'icon-calendar'
+    },
+  ];
+
 }
---
+++ src/app/directives/animated-counter.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appAnimatedCounter]',
+  standalone: true
+})
+export class AnimatedCounterDirective {
+
+  constructor() { }
+
+}
